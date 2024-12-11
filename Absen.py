2fbea3a70c7cf3c435a9bee669fad32d5ad3668d
Hinabil <139767756+Hinabil@users.noreply.github.com>
2024-12-11 11:46:37 +0700

    Update Absen.py

--- Absen.py
+++ Absen.py
@@ -1,6 +1,8 @@
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.common.keys import Keys 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 
 # Konfigurasi
