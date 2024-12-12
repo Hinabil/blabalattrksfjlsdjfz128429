12e789b2575366ec208cdfbb1635d03f2ad5f9a7
Hinabil <139767756+Hinabil@users.noreply.github.com>
2024-12-12 15:16:59 +0700

    Update Absen.py

--- Absen.py
+++ Absen.py
@@ -34,30 +34,30 @@ def otomatis_absen(nama, username, password):
         driver.get(url_login)
 
         # Tunggu elemen login tersedia
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, "//input[@class='form-control' and @placeholder='NIP/NPM']"))
         ).send_keys(username)
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, "//input[@class='form-control' and @placeholder='Password']"))
         ).send_keys(password)
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.XPATH, "//button[text()='Login']"))
         ).click()
-        print(f"Berhasil login untuk username: {username}")
+        print(f"Berhasil login untuk : {nama}")
 
         # Buka halaman absen
         driver.get(url_absen)
         # Klik tombol absen
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.XPATH, "//button[text()='Konfirmasi Kehadiran']"))
         ).click()
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.XPATH, "//button[text()='Konfirmasi']"))
         ).click()
 
         print(f"Absen berhasil untuk : {nama}")
     except TimeoutException:
-        print(f"Elemen tidak muncul untuk username {username}, absen gagal.")
+        print(f"Belum ada absen untuk {nama}, absen gagal.")
     except Exception as e:
         print(f"Terjadi kesalahan untuk {nama}: {e}")
 
