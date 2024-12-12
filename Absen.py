231f9592a20699cb7df7227783812c73f7befb54
Hinabil <139767756+Hinabil@users.noreply.github.com>
2024-12-12 14:53:44 +0700

    Update Absen.py

--- Absen.py
+++ Absen.py
@@ -4,10 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
+import csv
 
 # Konfigurasi
-username = "2404111010055"  # Ganti dengan username Anda
-password = "15927386"  # Ganti dengan password Anda
 url_login = "https://simkuliah.usk.ac.id/"  # URL halaman login
 url_absen = "https://simkuliah.usk.ac.id/index.php/absensi"  # URL halaman absen
 
@@ -22,35 +21,43 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 # Inisialisasi WebDriver
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-def otomatis_absen():
-    
+def otomatis_absen(nama, username, password):
     try:
         # Buka halaman login
         driver.get(url_login)
         time.sleep(2)
-        
+
         # Masukkan username dan password
         driver.find_element(By.XPATH, "//input[@class='form-control' and @placeholder='NIP/NPM']").send_keys(username)  # Ganti ID sesuai elemen di situs
         driver.find_element(By.XPATH, "//input[@class='form-control' and @placeholder='Password']").send_keys(password)  # Ganti ID sesuai elemen di situs
         driver.find_element(By.XPATH, "//button[text()='Login']").click()  # Ganti ID sesuai elemen di situs
         time.sleep(3)
-        print("berhasil masuk")
-        
+        print(f"Berhasil login untuk username: {nama}")
+
         # Buka halaman absen
         driver.get(url_absen)
         time.sleep(5)
-        
+
         # Klik tombol absen
         driver.find_element(By.XPATH, "//button[text()='Konfirmasi Kehadiran']").click()
         time.sleep(3)
-        driver.find_element(By.XPATH, "//button[text()='Konfirmasi']").click() 
+        driver.find_element(By.XPATH, "//button[text()='Konfirmasi']").click()
         time.sleep(3)
-        
-        print("Absen berhasil!")
-    except Exception as e:
-        print(f"Terjadi kesalahan: {e}")
-    finally:
-        driver.quit()
 
-# Jalankan fungsi
-otomatis_absen()
+        print(f"Absen berhasil untuk : {nama}")
+    except Exception as e:
+        print(f"Terjadi kesalahan untuk {nama}: {e}")
+
+# Membaca file CSV dan menjalankan otomatis absen untuk setiap user
+try:
+    with open("user.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            nama = row["nama"]
+            username = row["username"]
+            password = row["password"]
+            
+            otomatis_absen(nama, username, password)
+finally:
+    driver.quit()
+    print("Proses selesai.")
