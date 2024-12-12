1429a95dd7e8372c876e356f5683a3435abda21a
Hinabil <139767756+Hinabil@users.noreply.github.com>
2024-12-12 15:11:34 +0700

    Update Absen.py

--- Absen.py
+++ Absen.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 import csv
 
