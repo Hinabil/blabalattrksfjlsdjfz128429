048e0be25273403dbcfbc3d8017a3cf07468ea52
Hinabil <139767756+Hinabil@users.noreply.github.com>
2025-04-29 20:53:10 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,18 +6,14 @@ import os
 app = Flask(__name__,
             template_folder='templates',
             static_folder='static')
-
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-GITHUB_REPO = os.environ.get("GITHUB_REPO")
-
-
-def get_file_sha():
-    url = f"https://api.github.com/repos/{GITHUB_REPO}/contents/{FILE_PATH}"
-    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()["sha"]
-    return None
+# Koneksi ke NeonDB
+conn = psycopg2.connect(
+    host=os.environ.get("PGHOST"),
+    dbname=os.environ.get("PGDATABASE"),
+    user=os.environ.get("PGUSER"),
+    password=os.environ.get("PGPASSWORD")
+)
+cursor = conn.cursor()
 
 @app.route("/")
 def index():
